32841137d17797afe3b5133688ad73f5d79895f5
varsha2316 <63259008+varsha2316@users.noreply.github.com>
2020-09-05 18:24:00 -0400

    Add files via upload

--- djangodemo/asgi.py
+++ djangodemo/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for djangodemo project.
+ASGI config for DJangoToDo project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangodemo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DJangoToDo.settings')
 
 application = get_asgi_application()
--- djangodemo/wsgi.py
+++ djangodemo/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djangodemo project.
+WSGI config for DJangoToDo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangodemo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DJangoToDo.settings')
 
 application = get_wsgi_application()
